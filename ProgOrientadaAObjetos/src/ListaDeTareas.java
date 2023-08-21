c644bf51f3b5df2b47d19f70d2fa4bba25b3d6e2
Bautista Pereyra Buch <bautistapereyrabuch@gmail.com>
2023-08-21 13:09:41 -0300

    Punto 5 cambios

--- ProgOrientadaAObjetos/src/ListaDeTareas.java
+++ ProgOrientadaAObjetos/src/ListaDeTareas.java
@@ -38,4 +38,13 @@ public class ListaDeTareas<T> {
             return estado;
         }
     }
+    public void main(){
+        Tarea tarea1=new Tarea("Consultar repuesto del auto.",3,LocalDate.of(2023,8,20));
+        tarea1.tachar();
+        Tarea tarea2=new Tarea("Ir al supermercado mañana.",1,LocalDate.of(2023,8,22));
+        Tarea tarea3=new Tarea("Ir al cine a ver la nueva peli de Marvel.",1,LocalDate.of(2023,8,20));
+        System.out.printf("Tarea 1: %s\n",tarea1.mostrarTarea());
+        System.out.printf("Tarea 2: %s\n",tarea2.mostrarTarea());
+        System.out.printf("Tarea 3: %s\n",tarea3.mostrarTarea());
+    }
 }
