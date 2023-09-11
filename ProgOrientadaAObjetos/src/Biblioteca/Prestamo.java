510dfdb49b31aaf25df9ab9e4162255d373d18d8
Nahuel <tamburrinahuel@gmail.com>
2023-09-11 16:41:19 -0300

    Tratando de hacer algo

---
+++ ProgOrientadaAObjetos/src/Biblioteca/Prestamo.java
@@ -0,0 +1,32 @@
+package Biblioteca;
+
+import Fecha.Fecha;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Prestamo {
+
+    LocalDate fechaP;
+    LocalDate fechaD;
+
+    public Prestamo (String fechaDePrestamo, String fechaDeDev){
+        this.fechaP = convertir(fechaDePrestamo);
+        this.fechaD = convertir(fechaDeDev);
+    }
+
+    private LocalDate convertir(String fecha){
+        DateTimeFormatter convertidor=DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        return LocalDate.parse(fecha,convertidor);
+    }
+
+// Finalizar un prestamo
+
+    public void finalizarPrestamo (){
+        if (fechaD < fechaP){
+
+        }
+    }
+
+
+}
